8cc76afb448d8e1711ec88315bfc81e86002b158
saadati944 <saadati944@gmail.com>
2020-04-18 10:29:34 +0430

    Add project files.

---
+++ game_of_life/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_of_life
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
