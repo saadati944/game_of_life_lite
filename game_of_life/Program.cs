91c3c53948b2d7d00f60bbe2d6566079c53a96c5
saadati944 <saadati944@gmail.com>
2020-04-18 10:32:41 +0430

    Try to assign variables form given args.

--- game_of_life/Program.cs
+++ game_of_life/Program.cs
@@ -14,7 +14,14 @@ namespace game_of_life
 
         static void Main(string[] args)
         {
-            
+            //try to asign variables form given args.
+            if (args.Length > 0)
+                int.TryParse(args[0], out w);
+            if (args.Length > 1)
+                int.TryParse(args[0], out h);
+            if (args.Length > 2)
+                int.TryParse(args[0], out speed);
+
         }
     }
 }
