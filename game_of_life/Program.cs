c01a9a78d039926699966911a8ce53891712830f
saadati944 <saadati944@gmail.com>
2020-04-19 10:32:26 +0430

    Add living codes.

--- game_of_life/Program.cs
+++ game_of_life/Program.cs
@@ -21,9 +21,9 @@ namespace game_of_life
 
 
             //---------------- a simple test ------------------
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 20; i++)
             {
-                player p = new player((player.player_kind)(i % 5), "", new point(-1, -1), (player.player_direction)range.getRandFrom(0, 6), '\\', -1, -1, -1, -1, -1, true);
+                player p = new player((player.player_kind)(i % 5), "", new point(-1, -1), (player.player_direction)range.getRandFrom(0, 6), '\\', -1, -1, -1,-1, -1, -1, true);
                 world.players.Add(p);
                 Console.WriteLine("name : {0}\ncharacter : {7}\nposition : {8}\nkind : {1}\ndirection : {9}\nage : {2}\nmaxage : {3}\nweight : {4}\nhunger level : {5}\nis alive : {6}\n\n_________________________________________________\n\n", p.Name, p.Kind.ToString(), p.Age, p.MaxAge, p.Weight, p.Hunger, p.Alive.ToString(), p.Character, p.Position.ToString(), p.Direction.ToString());
             }
@@ -44,8 +44,8 @@ namespace game_of_life
                 world.draw();
                 Console.SetCursorPosition(0, 0);
                 Console.Write(world.convertBuffer());
-                Console.Title = $"generation : {world.generation} & year : {world.year}";
-                System.Threading.Thread.Sleep(20);
+                Console.Title = $"generation : {world.generation} & year : {world.year} & players : {world.players.Count}";
+                System.Threading.Thread.Sleep(world.__waitAfterNewGen);
             }
         }
 
@@ -55,25 +55,38 @@ namespace game_of_life
         public static char[,] buffer, nullBuffer;
         public static long generation = 0, year = 0;
         //public variables
-        public static int w = 60, h = 30, speed = 1;
+        public static int w = 30, h = 15, speed = 1;
         public static List<player> players = new List<player>();
         #region configValues
-        public static int __weightstart = 10, __weightend = 150, __agestart = 0, __hungerstart = 0, __hungerend = 10, __maxagestart = 10, __maxageend = 100, __namestart = 100000, __nameend = 900000, __genstoyear = 30;
+        public static int __weightstart = 10, __weightend = 150, __agestart = 0, __hungerstart = 0, __hungerend = 10, __maxagestart = 20, __maxageend = 100, __namestart = 100000, __nameend = 900000, __genstoyear = 20,__childstart=0,__childend=3,__maxPlayers=200,__waitAfterNewGen=0,__maxmaxage=100;
         public static char[] __defaultKindCharacters = { 'M', 'F', 'f', 'A', 'a' };
         #endregion
+
+        private static int[] kinds = { 0, 0, 0, 0, 0 };
         public static void nextGen()
         {
+            kinds = new int[]{ 0, 0, 0, 0, 0 };
             if (players.Count == 0)
             {
                 gameOver();
                 return;
             }
+
             List<player> toRemove = new List<player>();
+            List<player> toAdd = new List<player>();
             for (int i = 0; i < players.Count; i++)
             {
                 if (generation % __genstoyear == 0)
-                    players[i].Age++;
-                if (players[i].Weight == 0)
+                {
+                    if(players[i].Age<__maxmaxage)
+                    players[i].Age++; 
+                if(players[i].Kind!=player.player_kind.animalMale|| players[i].Kind != player.player_kind.animalFemale)
+                        players[i].Age++;
+                }
+
+
+                //remove dead players
+                if (players[i].Weight <= 1)
                 {
                     toRemove.Add(players[i]);
                     continue;
@@ -85,10 +98,12 @@ namespace game_of_life
                     {
                         case player.player_kind.male:
                             players[i].Kind = player.player_kind.food;
+                            players[i].Character = __defaultKindCharacters[2];
                             players[i].Age = 0;
                             break;
                         case player.player_kind.female:
                             players[i].Kind = player.player_kind.food;
+                            players[i].Character = __defaultKindCharacters[2];
                             players[i].Age = 0;
                             break;
                         case player.player_kind.food:
@@ -97,10 +112,12 @@ namespace game_of_life
                         //break;
                         case player.player_kind.animalMale:
                             players[i].Kind = player.player_kind.food;
+                            players[i].Character = __defaultKindCharacters[2];
                             players[i].Age = 0;
                             break;
                         case player.player_kind.animalFemale:
                             players[i].Kind = player.player_kind.food;
+                            players[i].Character = __defaultKindCharacters[2];
                             players[i].Age = 0;
                             break;
                         default:
@@ -109,6 +126,126 @@ namespace game_of_life
                 }
                 if (players[i].Alive == false)
                     continue;
+                if (players[i].Kind != player.player_kind.food && players[i].Weight == __weightend)
+                {
+                    players[i].MaxAge += 1;
+                    players[i].Weight = (int)(players[i].Weight*0.9);
+                    players[i].Child++;
+                }
+                //living.
+                switch (players[i].Kind)
+                {
+                    case player.player_kind.male:
+                        foreach (player  x in players)
+                        {
+                            if (x.Kind == player.player_kind.animalMale || x.Kind == player.player_kind.male || x.Kind == player.player_kind.animalFemale||x.Position.x!=players[i].Position.x|| x.Position.y != players[i].Position.y)
+                                continue;
+                            if (x.Kind == player.player_kind.food && players[i].Weight < __weightend)
+                            {
+                                if (x.Weight <= __weightend - players[i].Weight)
+                                {
+                                    players[i].Weight += x.Weight;
+                                    x.Weight = 0;
+                                }
+                                else 
+                                {
+                                    players[i].Weight += __weightend - players[i].Weight;
+                                    x.Weight -= __weightend - players[i].Weight;
+                                }
+                            }
+                            else if(__maxPlayers>players.Count&&players[i].Child>0&&x.Kind == player.player_kind.female&&x.Child>0)
+                            {
+
+                                player p = new player((player.player_kind)range.getRandFrom(0,1), "", new point(-1, -1), (player.player_direction)range.getRandFrom(0, 6), '\\', 0, 0, -1, 0, x.Weight/2+players[i].Weight/2, -1, true);
+                                toAdd.Add(p);
+                                players[i].Child--;
+                                players[i].Weight = players[i].Weight / 2;
+                                    x.Child--;
+                                x.Weight = x.Weight / 2;
+                                if (players[i].Weight == 1)
+                                    toRemove.Add(players[i]);
+                                if (x.Weight == 1)
+                                    toRemove.Add(x);
+                            }
+                        }
+                        break;
+                    case player.player_kind.female:
+                        foreach (player x in players)
+                        {
+                            if (x.Kind == player.player_kind.animalMale || x.Kind == player.player_kind.female || x.Kind == player.player_kind.male || x.Kind == player.player_kind.animalFemale || x.Position.x != players[i].Position.x || x.Position.y != players[i].Position.y)
+                                continue;
+                            if (x.Kind == player.player_kind.food && players[i].Weight < __weightend)
+                            {
+                                if (x.Weight <= __weightend - players[i].Weight)
+                                {
+                                    players[i].Weight += x.Weight;
+                                    x.Weight = 0;
+                                }
+                                else
+                                {
+                                    players[i].Weight += __weightend - players[i].Weight;
+                                    x.Weight -= __weightend - players[i].Weight;
+                                }
+                            }
+                        }
+                        break;
+                    case player.player_kind.animalMale:
+                        foreach (player x in players)
+                        {
+                            if (x.Kind == player.player_kind.animalMale  || x.Position.x != players[i].Position.x || x.Position.y != players[i].Position.y)
+                                continue;
+                            if (x.Kind == player.player_kind.food/*|| x.Kind == player.player_kind.male|| x.Kind == player.player_kind.female */&& players[i].Weight < __weightend)
+                            {
+                                if (x.Weight <= __weightend - players[i].Weight)
+                                {
+                                    players[i].Weight += x.Weight;
+                                    x.Weight = 0;
+                                }
+                                else
+                                {
+                                    players[i].Weight += __weightend - players[i].Weight;
+                                    x.Weight -= __weightend - players[i].Weight;
+                                }
+                            }
+                            else if (__maxPlayers > players.Count && players[i].Child > 0 && x.Kind == player.player_kind.animalFemale && x.Child > 0)
+                            {
+
+                                player p = new player((player.player_kind)range.getRandFrom(3, 4), "", new point(-1, -1), (player.player_direction)range.getRandFrom(0, 6), '\\', 0, 0, -1, 0, x.Weight / 2 + players[i].Weight / 2, -1, true);
+                                toAdd.Add(p);
+                                    players[i].Child--;
+                                players[i].Weight = players[i].Weight / 2;
+                                    x.Child--;
+                                x.Weight = x.Weight / 2;
+                                if (players[i].Weight == 1)
+                                    toRemove.Add(players[i]);
+                                if(x.Weight==1)
+                                    toRemove.Add(x);
+                            }
+                        }
+                        break;
+                    case player.player_kind.animalFemale:
+                        foreach (player x in players)
+                        {
+                            if (x.Kind == player.player_kind.animalFemale || x.Kind == player.player_kind.animalMale || x.Position.x != players[i].Position.x || x.Position.y != players[i].Position.y)
+                                continue;
+                            if (x.Kind == player.player_kind.food /*|| x.Kind == player.player_kind.male || x.Kind == player.player_kind.female*/ && players[i].Weight < __weightend)
+                            {
+                                if (x.Weight <= __weightend - players[i].Weight)
+                                {
+                                    players[i].Weight += x.Weight;
+                                    x.Weight = 0;
+                                }
+                                else
+                                {
+                                    players[i].Weight += __weightend - players[i].Weight;
+                                    x.Weight -= __weightend - players[i].Weight;
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
 
                 //moving codes
                 if (players[i].Kind != player.player_kind.food)
@@ -302,12 +439,22 @@ namespace game_of_life
                         default:
                             break;
                     }
+
+                //loging
+                kinds[(int)players[i].Kind]++;
+                kinds[2] = players.Count - kinds.Sum() + kinds[2];
+                if (kinds.Sum() == kinds[2])
+                    kinds[2] = players.Count;
             }
+
+            Console.WriteLine($"\nmales : {kinds[0]} , females : {kinds[1]} , foods : {kinds[2]} , animalMale : {kinds[3]} , animalFemale : {kinds[4]}                      ");
+
             generation++;
             if (generation % __genstoyear == 0)
                 ++year;
             foreach (player x in toRemove)
                 players.Remove(x);
+            players.AddRange(toAdd);
         }
         public static void gameOver()
         {
@@ -377,7 +524,7 @@ namespace game_of_life
 
         public string Name;
         public bool Alive;
-        public int Age, Hunger, Weight, MaxAge;
+        public int Age, Hunger, Weight, MaxAge,Child;
         public int Injure;
         public player_kind Kind;
         public player_direction Direction;
@@ -388,7 +535,7 @@ namespace game_of_life
         //"" for string values will replace randomly
         //'\' for character values will replace randomly
         //(-1,-1) for points will replace randomly
-        public player(player_kind kind, string name, point position, player_direction direction, char character = '\\', int age = 0, int injure = 0, int hunger = 0, int weight = -1, int maxAge = -1, bool alive = true)
+        public player(player_kind kind, string name, point position, player_direction direction=player_direction.nw, char character = '\\', int age = 0, int injure = 0,int child=2, int hunger = 0, int weight = -1, int maxAge = -1, bool alive = true)
         {
             range r = new range(0, 1);
 
@@ -424,6 +571,16 @@ namespace game_of_life
                 r.Start = world.__agestart;
                 r.End = MaxAge;
                 Age = r.getRandFrom();
+            }
+
+            //assign childs count;
+            if (child > -1)
+                Child = child;
+            else
+            {
+                r.Start = world.__childstart;
+                r.End = world.__childend;
+                Child = r.getRandFrom();
             }
 
             //assign injure level
